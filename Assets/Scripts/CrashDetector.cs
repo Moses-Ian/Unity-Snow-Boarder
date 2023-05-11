da905a6ef336eff7eb9a4572bbba740040640926
Ian Moses <ian.moses@cognizant.com>
2023-05-11 09:48:23 -0700

    particle effects done

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float ReloadDelay = .5f;
+    [SerializeField] ParticleSystem HitEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ground")
         {
+            HitEffect.Play();
             Invoke("ReloadScene", ReloadDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float ReloadDelay = 1f;
+    [SerializeField] ParticleSystem FinishEffect;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            FinishEffect.Play();
             Invoke("ReloadScene", ReloadDelay);
         }
     }
